54acdca1ecaf5e450bb910af2e892ebfc6f1769d
Dawit Mekonnen <dawityitagesu@gmail.com>
2022-03-16 17:17:37 +0300

    space b/n input fields

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,48 +3,47 @@
         <x-slot name="logo">
             <x-jet-authentication-card-logo />
         </x-slot>
-
-        <x-jet-validation-errors class="mb-3" />
         <div class="w-100">
             <x-slot name="logo">
                 <x-jet-authentication-card-logo />
             </x-slot>
             <div class="register-box-body">
-                <p class="login-box-msg">Volunteer Registration Form</p>
+                <h5 class="text-center  p-4">Volunteer Registration Form</h5>
                 <x-jet-validation-errors class="mb-3" />
-                <form action="/register.php" method="post" id="registrationForm">
-                    <div class="form-group has-feedback">
-                        <input type="text" class="form-control" placeholder="First name" name="fname" id="fnameInput"
-                            autofocus="">
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
+                    <div class="mb-3 has-feedback">
+                        <input type="text" class="form-control" placeholder="First name" name="name" id="name"
+                            autofocus="" value="{{old('name')}}">
                         <span class="glyphicon glyphicon-user form-control-feedback"></span>
                     </div>
-                    <div class="form-group has-feedback">
-                        <input type="text" class="form-control" placeholder="Last name" name="lname" id="lnameInput">
+                    <div class="mb-3 has-feedback">
+                        <input type="text" class="form-control" placeholder="Last name" name="lastname" id="lastname" value="{{old('lastname')}}">
                         <span class="glyphicon glyphicon-user form-control-feedback"></span>
                     </div>
-                    <div class="form-group has-feedback">
-                        <input type="email" class="form-control" placeholder="Email" name="email" id="emailInput">
+                    <div class="mb-3 has-feedback">
+                        <input type="email" class="form-control" placeholder="Email" name="email" id="email" value="{{old('email')}}">
                         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
                     </div>
-                    <div class="form-group has-feedback">
+                    <div class="mb-3 has-feedback">
                         <input type="tel" class="form-control" placeholder="Phone Number" name="phone"
-                            id="phoneInput">
+                            id="phone" value="{{old('phone')}}">
                         <span class="glyphicon glyphicon-phone form-control-feedback"></span>
                     </div>
-                    <div class="form-group has-feedback">
+                    <div class="mb-3 has-feedback">
                         <input type="text" class="form-control" placeholder="Usename" name="username"
-                            id="usernameInput">
+                            id="username" value="{{old('username')}}">
                         <span class="glyphicon glyphicon-user form-control-feedback"></span>
                     </div>
-                    <div class="form-group has-feedback">
+                    <div class="mb-3 has-feedback">
                         <input type="password" class="form-control" name="password" placeholder="Password">
                         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
                     </div>
-                    <div class="form-group has-feedback">
-                        <input type="password" class="form-control" name="repassword" placeholder="Retype password">
+                    <div class="mb-3 has-feedback">
+                        <input type="password" class="form-control" name="password_confirmation" placeholder="Retype password">
                         <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
                     </div>
-                    <div class="form-group">
+                    <div class="mb-3">
 
                         <select class="form-control" name="userSex">
                             <option value="">Select Sex</option>
